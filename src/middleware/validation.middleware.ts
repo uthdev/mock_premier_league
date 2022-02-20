971e64086f44742634ed28e4ec706a9e44b18d2f
uthdev <adelekegbolahan92@yahoo.com>
2022-02-20 22:35:56 +0100

    fix deployment bug

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,5 +1,5 @@
 import { plainToInstance } from 'class-transformer';
-import { validate, ValidationError } from 'class-validator';
+import { validate } from 'class-validator';
 import { RequestHandler } from 'express';
 import HttpException from '../exceptions/HttpException';
 
@@ -12,9 +12,9 @@ import HttpException from '../exceptions/HttpException';
 function validationMiddleware<T>(type: any, skipMissingProperties = false): RequestHandler {
   return (req, res, next) => {
     validate(plainToInstance(type, req.body), { skipMissingProperties })
-      .then((errors: ValidationError[]) => {
+      .then((errors:any) => {
         if (errors.length > 0) {
-          const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
+          const message = errors.map((error: any) => Object.values(error.constraints)).join(', ');
           next(new HttpException(400, message));
         } else {
           next();
